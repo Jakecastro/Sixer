be2de306444019943d8781e7a0ac2b9fb82a0b89
dsmith510 <d.smith510@yahoo.com>
2015-11-06 12:54:55 -0800

    most recent changes to login

--- Sixer/LoginViewController.m
+++ Sixer/LoginViewController.m
@@ -6,12 +6,21 @@
 //  Copyright © 2015 Jake Castro. All rights reserved.
 //
 
+// Imports
 #import "LoginViewController.h"
+#import <Parse/Parse.h>
+#import "ExerciseViewController.h"
 
-@interface LoginViewController ()
+// Delegates
+@interface LoginViewController () <UITextFieldDelegate>
+
+//Outlets
 @property (weak, nonatomic) IBOutlet UIImageView *usernameIconImageView;
 @property (weak, nonatomic) IBOutlet UIImageView *passwordIconImageView;
 @property (weak, nonatomic) IBOutlet UIButton *loginButton;
+
+// Properties
+@property ExerciseViewController *exerciseViewController;
 
 @end
 
@@ -19,6 +28,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+// set delegates
+    
+    self.
     
 }
 
