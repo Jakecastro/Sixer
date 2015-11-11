18ccbb5d684636f546f8b04acd84ea3a1feabd59
Jake Castro <Jake@Jakes-MacBook-Pro.local>
2015-11-10 16:03:15 -0800

    updating color.h

---
+++ Sixer/Color.h
@@ -0,0 +1,31 @@
+//
+//  Color.h
+//  Sixer
+//
+//  Created by Jake Castro on 11/10/15.
+//  Copyright © 2015 Jake Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Color : UIColor
+
++ (UIColor *)hourOrangeColor;
++ (UIColor *)hourHighlightBorderOrangeColor;
+
++ (UIColor *)hourYellowTextColor;
+
++ (UIColor *)hourGreenColor;
++ (UIColor *)hourDarkGreenColor;
+
++ (UIColor *)hourDarkBlueColor;
++ (UIColor *)hourBackgroundBlueColor;
++ (UIColor *)hourTimerDarkBlue;
++ (UIColor *)hourTimerBlue;
+
++ (UIColor *)hourNormalStateBorderColor;
+
+
+
+
+@end
